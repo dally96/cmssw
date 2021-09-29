245a85eb73c34b6eba94e40d05de79ae487da857
Thomas Schuh <thomas.schuh@stfc.ac.uk>
2021-09-29 11:55:09 +0100

    comments for ian

--- SimTracker/TrackTriggerAssociation/python/StubAssociator_cfi.py
+++ SimTracker/TrackTriggerAssociation/python/StubAssociator_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 StubAssociator_params = cms.PSet (
-  UseTTStubAssMap         = cms.bool    ( False ),                                                   #
+  UseTTStubAssMap         = cms.bool    ( True  ),                                                  # association is either taken from TTStubAssociationMap (true) or from TTClusterAssociationMap (false)
   InputTagTTStubDetSetVec = cms.InputTag( "TTStubsFromPhase2TrackerDigis",     "StubAccepted"    ), #
   InputTagTTClusterAssMap = cms.InputTag( "TTClusterAssociatorFromPixelDigis", "ClusterAccepted" ), #
   InputTagTTStubAssMap    = cms.InputTag( "TTStubAssociatorFromPixelDigis",    "StubAccepted"    ), #
