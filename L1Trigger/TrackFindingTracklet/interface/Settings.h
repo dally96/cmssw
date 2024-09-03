ed686882b2328862f7aa76f92b5ab0e76c572b52
Daniel Ally <d.ally@cern.ch>
2024-09-03 22:49:24 +0200

    Newer version of DR where tracks are ordered by seed rank and duplicate tracks are skipped in the comparison

--- L1Trigger/TrackFindingTracklet/interface/Settings.h
+++ L1Trigger/TrackFindingTracklet/interface/Settings.h
@@ -448,15 +448,15 @@ namespace trklet {
     //have the factor if 2
     double krprojshiftdisk() const { return 2 * kr(); }
 
-    double benddecode(unsigned int ibend, unsigned int layerdisk, bool isPSmodule) const {
+    double benddecode(int ibend, int layerdisk, bool isPSmodule) const {
       if (layerdisk >= N_LAYER && (!isPSmodule))
-        layerdisk += N_DISK;
+        layerdisk += (N_LAYER - 1);
       double bend = benddecode_[layerdisk][ibend];
       assert(bend < 99.0);
       return bend;
     }
 
-    double bendcut(unsigned int ibend, unsigned int layerdisk, bool isPSmodule) const {
+    double bendcut(int ibend, int layerdisk, bool isPSmodule) const {
       if (layerdisk >= N_LAYER && (!isPSmodule))
         layerdisk += N_DISK;
       double bendcut = bendcut_[layerdisk][ibend];
@@ -903,9 +903,7 @@ namespace trklet {
         {"TRE", 108},
         {"DR", 108}};  //Specifies how many tracks allowed per bin in DR
 
-    // If set to true this creates txt files, which the ROOT macros in
-    // https://github.com/cms-L1TK/TrackPerf/tree/master/PatternReco
-    // can then use to study truncation of individual algo steps within tracklet chain.
+    // If set to true this will generate debub printout in text files
     std::unordered_map<std::string, bool> writeMonitorData_{{"IL", false},
                                                             {"TE", false},
                                                             {"CT", false},
@@ -1080,8 +1078,6 @@ namespace trklet {
     double stripLength_PS_{0.1467};
     double stripLength_2S_{5.0250};
 
-    // The DR binning below disabled, as doesn't match latest FW.
-
     //Following values are used for duplicate removal
     //Only one bin currently used.
     std::vector<double> rinvBins_{-rinvcut(), rinvcut()};
@@ -1091,7 +1087,7 @@ namespace trklet {
     //Overlap size for the overlap phi bins in DR
     double phiOverlapSize_{M_PI / 360};
     //The maximum number of tracks that are compared to all the other tracks per rinv bin
-    int numTracksComparedPerBin_{9999};
+    int numTracksComparedPerBin_{32};
 
     double sensorSpacing_2S_{0.18};
   };
