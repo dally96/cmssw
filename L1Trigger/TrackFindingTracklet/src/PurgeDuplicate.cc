4094c7832f8fb8556f85f2396527877d4160e648
Daniel Ally <d.ally@cern.ch>
2025-01-30 21:41:58 +0100

    Different comparison and merging behavior in PurgeDuplicate for extended seeds

--- L1Trigger/TrackFindingTracklet/src/PurgeDuplicate.cc
+++ L1Trigger/TrackFindingTracklet/src/PurgeDuplicate.cc
@@ -294,8 +294,14 @@ void PurgeDuplicate::execute(std::vector<Track>& outputtracks, unsigned int iSec
             if (nShareLay >= settings_.minIndStubs()) {  // For number of shared stub merge condition
               dupMap[seedRankIdx[itrk]][seedRankIdx[jtrk]] = true;
               dupMap[seedRankIdx[jtrk]][seedRankIdx[itrk]] = true;
-              if (seedRank[itrk] <= seedRank[jtrk]) {
-                mergedTrack[seedRankIdx[jtrk]] = true;
+              if ((seedRank[seedRankIdx[itrk]] == 9) && (seedRank[seedRankIdx[jtrk]] == 9)) {
+                if (seedRank[seedRankIdx[itrk]] < seedRank[seedRankIdx[jtrk]]) {
+                  mergedTrack[seedRankIdx[jtrk]] = true;
+                }
+              } else {
+                if (seedRank[seedRankIdx[itrk]] <= seedRank[seedRankIdx[jtrk]]) {
+                  mergedTrack[seedRankIdx[jtrk]] = true;
+                }
               }
             }
           }
@@ -307,12 +313,17 @@ void PurgeDuplicate::execute(std::vector<Track>& outputtracks, unsigned int iSec
               // Set preferred track based on seed rank
               int preftrk;
               int rejetrk;
-              if (seedRank[seedRankIdx[itrk]] <= seedRank[seedRankIdx[jtrk]]) {
-                preftrk = itrk;
-                rejetrk = jtrk;
-              } else {
+              if ((seedRank[seedRankIdx[itrk]] == 9) && (seedRank[seedRankIdx[jtrk]] == 9)) {
                 preftrk = jtrk;
                 rejetrk = itrk;
+              } else {
+                if (seedRank[seedRankIdx[itrk]] <= seedRank[seedRankIdx[jtrk]]) {
+                  preftrk = itrk;
+                  rejetrk = jtrk;
+                } else {
+                  preftrk = jtrk;
+                  rejetrk = itrk;
+                }
               }
 
               // If the preffered track is in more than one bin, but not in the proper rinv or phi bin, then mark as true
